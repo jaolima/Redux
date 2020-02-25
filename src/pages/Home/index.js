1d943ecac88ce3bceb93410ec1e280644a4ee46d
jaolima <joaolimafonseca2@gmail.com>
2020-02-24 22:16:00 -0300

    finalizando o css

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,94 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+          <img 
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-corre-1/08/D22-3707-008/D22-3707-008_zoom2.jpg?ims=326x" 
+          alt=""
+          />
+          <strong>Tênis muito doido</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+                <MdAddShoppingCart size={16} color="#FFF"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+      </li>
+
+      <li>
+          <img 
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-corre-1/08/D22-3707-008/D22-3707-008_zoom2.jpg?ims=326x" 
+          alt=""
+          />
+          <strong>Tênis muito doido</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+                <MdAddShoppingCart size={16} color="#FFF"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+      </li>
+
+      <li>
+          <img 
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-corre-1/08/D22-3707-008/D22-3707-008_zoom2.jpg?ims=326x" 
+          alt=""
+          />
+          <strong>Tênis muito doido</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+                <MdAddShoppingCart size={16} color="#FFF"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+      </li>
+
+      <li>
+          <img 
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-corre-1/08/D22-3707-008/D22-3707-008_zoom2.jpg?ims=326x" 
+          alt=""
+          />
+          <strong>Tênis bonitim</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+                <MdAddShoppingCart size={16} color="#FFF"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+      </li>
+
+      <li>
+          <img 
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-corre-1/08/D22-3707-008/D22-3707-008_zoom2.jpg?ims=326x" 
+          alt=""
+          />
+          <strong>Tênis muito doido</strong>
+          <span>R$129,90</span>
+
+          <button type="button">
+            <div>
+                <MdAddShoppingCart size={16} color="#FFF"/> 3
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+      </li>
+    </ProductList>
   );
 }
